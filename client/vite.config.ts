5f732996d7cb08fcc70d0c449fd1013e33b3e4be
thomasr10 <t.rivoire01@gmail.com>
2026-04-29 11:40:02 +0200

    set up vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    watch: {
+      usePolling: true,
+      interval: 500
+    },
+    host: '0.0.0.0',
+    port: 3000
+  },
   plugins: [react()],
 })
