a93cdcfbdcb2bfc6f80bdc8e8780a21a76d5885b
thomasr10 <t.rivoire01@gmail.com>
2026-05-27 17:18:54 +0200

    ✨ added email in token instead of username + added serializer component ✨

--- server/src/Controller/AuthController.php
+++ server/src/Controller/AuthController.php
@@ -8,6 +8,10 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
 use App\Service\AuthService;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
 
 final class AuthController extends AbstractController
 {   
@@ -58,5 +62,33 @@ final class AuthController extends AbstractController
         return $this->json([
             'message' => 'Utilisateur créé avec succès'
         ]);
+    }
+
+    #[Route('/api/me', methods: ['GET'])]
+    public function me(): JSONResponse
+    {
+        $user = $this->getUser();
+        $normalizer = new ObjectNormalizer();
+        $encoder = new JsonEncoder();
+        $serializer = new Serializer([$normalizer], [$encoder]);
+
+        if(!$user) {
+            return $this->json([
+                'message' => 'Utilisateur non trouvé'
+            ], 401);
+        }
+
+        return $this->json([
+            'message' => 'Utilisateur récupéré',
+            'user' => $serializer->serialize($user, 'json', [AbstractNormalizer::IGNORED_ATTRIBUTES => ['age']])
+        ]);
+    }
+
+    #[Route('/api/logout', methods: ['GET'])]
+    public function logout(): JsonResponse
+    {
+        $response = new JsonResponse(['message' => 'Utilisateur déconnecté avec succès']);
+        $response->headers->clearCookie('token');
+        return $response;
     }
 }
